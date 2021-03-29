b2f06a55340d7422b5f67432c5b16be556115e0a
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-29 18:44:24 +0300

    inter commit

--- user/user.patch.js
+++ user/user.patch.js
@@ -2,15 +2,21 @@ const fs = require('fs');
 const express = require('express');
 const Router = express.Router();
 const dataFile = './data.json';
+const { body, validationResult, check } = require('express-validator');
+ 
 
-const patch = Router.patch('/:id', (req, res) => {
+const patch = Router.patch('/:id',
+
+    body('name').optional().isString().trim(),
+    body('done').optional().isBoolean(), 
+    (req, res) => {
     let newTasks = fs.readFileSync(dataFile, 'utf-8');
     const todo = JSON.parse(newTasks);
-    const item = todo.some(e => e.id === req.params.id);
+    const item = todo.some(e => e.id == req.params.id);
     
     if(!item) return res.status(404).send('Item not found');
 
-    const editingItem = todo.find(e => e.id === req.params.id);
+    const editingItem = todo.find(e => e.id == req.params.id);
     editingItem.name = req.body.name;
 
     if(!item) return res.status(404).send('Item not exists');
@@ -19,7 +25,7 @@ const patch = Router.patch('/:id', (req, res) => {
 
     const stringifiedTasks = JSON.stringify(todo)
     fs.writeFileSync(dataFile, stringifiedTasks);
-
+  
 
     res.send(editingItem); 
 
