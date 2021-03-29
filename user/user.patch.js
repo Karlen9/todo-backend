923e3a7117ac2579ac1ce1b06281b68fa48e85ca
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-29 17:46:44 +0300

    Patch endpoint added

--- user/user.patch.js
+++ user/user.patch.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
 const express = require('express');
 const Router = express.Router();
+const dataFile = './data.json';
 
-const patch = Router.patch('user/', (req, res) => {
+const patch = Router.patch('/:id', (req, res) => {
+    let newTasks = fs.readFileSync(dataFile, 'utf-8');
+    const todo = JSON.parse(newTasks);
+    const item = todo.some(e => e.id === req.params.id);
+    
+    if(!item) return res.status(404).send('Item not found');
+
+    const editingItem = todo.find(e => e.id === req.params.id);
+    editingItem.name = req.body.name;
+
+    if(!item) return res.status(404).send('Item not exists');
+
+    editingItem.done = req.body.done;
+
+    const stringifiedTasks = JSON.stringify(todo)
+    fs.writeFileSync(dataFile, stringifiedTasks);
+
+
+    res.send(editingItem); 
+
 
 });
 
