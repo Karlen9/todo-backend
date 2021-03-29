f4b6999efca9a1376a571ddee859e1cf4359877c
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-29 15:58:13 +0300

    Get endpoint added

--- user/user.get.js
+++ user/user.get.js
@@ -1,9 +1,38 @@
+const { parse } = require('dotenv/types');
 const express = require('express');
-const Router = express.Router()
+const Router = express.Router();
+const fs = require('fs');
+const dataFile = './data.json';
 
 
-const get = Router.get('/tasks', (req, res) => {
-    res.send(tasks.name);
-  })
+const get = Router.get('/', (req, res) => {
+  let tasks = fs.readFileSync(dataFile, 'utf-8');
+  const parsedTasks = JSON.parse(tasks);
+  let newTasks = [];
+
+  switch(req.query.sort) {
+    case "asc": 
+      newTasks = parsedTasks;
+      break;
+    case "desc":
+      newTasks = parsedTasks.reverse();
+      break;
+    default: newTasks = parsedTasks; 
+      break;
+  }
+
+  switch(req.query.filterBy) {
+    case "done":
+      newTasks = parsedTasks.filter(e => e.done === true);
+      break;
+    case "undone":
+      newTasks = parsedTasks.filter(e => e.done === false)
+    default: newTasks = parsedTasks;
+      break;
+  }
+
+
+  res.send(newTasks);
+})
 
 module.exports = get;
