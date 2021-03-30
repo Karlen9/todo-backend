c4310eae1816fb66299d57e4d9652775855bfc88
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-30 17:10:33 +0300

    CORS added

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const { fileURLToPath } = require('url');
 const filePath = './data.json'
+const cors = require('cors');
 
 const app = express();
 const initialFileArray = JSON.stringify([]);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cors())
 app.use('/task', require('./user/user.post'));
 app.use('/task', require('./user/user.delete'));
 app.use('/tasks', require('./user/user.get'));
@@ -26,7 +27,6 @@ if (fs.existsSync(filePath)){
 
 }
 
-  
 
 
 app.listen(process.env.PORT || 3000, () => {
