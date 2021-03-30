6ce4138021fccd8ea4e64559031dbf3c69505131
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-30 14:45:55 +0300

    File creating

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const { fileURLToPath } = require('url');
+const filePath = '/data.json'
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/task', require('./user/user.post'))
-app.use('/task', require('./user/user.delete'))
-app.use('/tasks', require('./user/user.get'))
-app.use('/task', require('./user/user.patch'))
+app.use('/task', require('./user/user.post'));
+app.use('/task', require('./user/user.delete'));
+app.use('/tasks', require('./user/user.get'));
+app.use('/task', require('./user/user.patch'));
+
+fs.openSync(filePath, 'w');
+fs.writeFileSync(filePath, []);
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('start');
