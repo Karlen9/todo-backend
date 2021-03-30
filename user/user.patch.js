316d283f20b8b436db8ef95d0af6bd6f59083f50
Karlen Misakyan <misakyankarlen@gmail.com>
2021-03-30 11:15:28 +0300

    Patch enndpoint added

--- user/user.patch.js
+++ user/user.patch.js
@@ -1,35 +1,40 @@
 const fs = require('fs');
 const express = require('express');
 const Router = express.Router();
+const { json } = require('body-parser');
 const dataFile = './data.json';
 const { body, validationResult, check } = require('express-validator');
  
 
 const patch = Router.patch('/:id',
 
-    body('name').optional().isString().trim(),
+    body('name').optional().isString(),
     body('done').optional().isBoolean(), 
-    (req, res) => {
+    (req, res) => {     
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()[0].msg });
+        
+    } else {
     let newTasks = fs.readFileSync(dataFile, 'utf-8');
-    const todo = JSON.parse(newTasks);
-    const item = todo.some(e => e.id == req.params.id);
-    
+    const todos = JSON.parse(newTasks);
+    const item = todos.some(e => e.id === req.params.id);
+    const todo = todos.find(todo => todo.id === req.params.id);
+    console.log(todo);
+
     if(!item) return res.status(404).send('Item not found');
 
-    const editingItem = todo.find(e => e.id == req.params.id);
-    editingItem.name = req.body.name;
+    const editingItem = todos.find(e => e.id === req.params.id);
+    editingItem.name = req.body.name ? req.body.name : todo.name;
+    editingItem.done = req.body.done ? req.body.done : todo.done;
 
-    if(!item) return res.status(404).send('Item not exists');
-
-    editingItem.done = req.body.done;
-
-    const stringifiedTasks = JSON.stringify(todo)
+    const stringifiedTasks = JSON.stringify(todos)
     fs.writeFileSync(dataFile, stringifiedTasks);
-  
 
     res.send(editingItem); 
 
-
+    }
 });
 
 
