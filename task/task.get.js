2b7893994ca6df6af2f1df883ab7d5c2dcaa8b4a
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-02 12:04:31 +0300

    Delete extra commit

--- task/task.get.js
+++ task/task.get.js
@@ -10,34 +10,6 @@ const get = Router.get('/', async (req, res) => {
   req.query.filterBy ? filter = {done: req.query.filterBy} : filter = {};
   req.query.sort ? sort = ['createdAt', req.query.sort] : sort = [];
   const tasks = await Task.findAll({ where: filter, order: sort })
-  
-  //let tasks = fs.readFileSync(dataFile, 'utf-8');
-  //const parsedTasks = JSON.parse(tasks);
-  
-  //const newTasks = [...DBtasks.rows];
-
-  // switch(req.query.sort) {
-  //   case 'asc': 
-  //     newTasks = newTasks;
-  //     break;
-  //   case 'desc':
-  //     newTasks = tasks.reverse(); 
-  //     break; 
-  // } 
-   
-  // switch(req.query.filterBy) {
-  //   case 'all':
-  //     newTasks = tasks;
-  //     break;
-  //   case 'done':
-  //     newTasks = tasks.filter(e => e.done === true);
-  //     break; 
-  //   case 'undone':
-  //     newTasks = tasks.filter(e => e.done === false)
-  //   default: 
-  //     break; 
-  // }
-
 
   res.send(tasks);
 })
