09efa87a0fb77fab46170a7d3a5e0f721a665396
= <=>
2021-04-04 12:34:39 +0300

    Edited delete controller

--- task/task.delete.js
+++ task/task.delete.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const Router = express.Router();
-const fs = require('fs');
 const Task = require('../models');
 
 
 const deleteTask = Router.delete('/:id', async (req, res) => {
+  try {
+    const task = await Task.findOne({where: {id: req.params.id}});
+    if(!task) throw new Error('Task does not exist');
+
+    const deletingTask = await Task.destroy({where: {id: req.params.id}}); 
   
-
-  const task = await Task.findOne({where: {id: req.params.id}});
-  if(task) {
-  const deletingTask = await Task.destroy({where: {id: req.params.id}}); 
-
-  }
   res.send(deletingTask);
-})
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+  
+});
 
 module.exports = deleteTask;
--- task/task.get.js
+++ task/task.get.js
@@ -5,7 +5,6 @@ const { Task } = require("../models");
 const get = Router.get("/", async (req, res) => {
   let filter = {};
   let sort = [];
-  let page = 1;
   if (req.query.filterBy) filter = { done: req.query.filterBy };
   if (req.query.sort) sort = ["createdAt", req.query.sort];
   const tasks = await Task.findAndCountAll({
