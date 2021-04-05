65d58293e301d1ec944dcdf7c38c708b3eb77264
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-05 15:09:24 +0300

    Actual version

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const fs = require("fs");
-const { fileURLToPath } = require("url");
 const cors = require("cors");
 
 const app = express();
-//const initialFileArray = JSON.stringify([]);
 
 const db = require("./models");
 const { sequelize } = require("./models");
@@ -22,13 +19,3 @@ app.use("/task", require("./task/task.post"));
 app.use("/task", require("./task/task.delete"));
 app.use("/tasks", require("./task/task.get"));
 app.use("/task", require("./task/task.patch"));
-
-// if (fs.existsSync(filePath)){
-//   console.log('Path exists');
-// } else {
-//   fs.open(filePath, 'w', (error) => {
-//     console.log(error);
-//   });
-//   fs.writeFileSync(filePath, initialFileArray);
-
-// }
