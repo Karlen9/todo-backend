e86318c776353dabb53e166e3246512600169bd5
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-05 14:41:28 +0300

    Deploy commit

--- task/task.delete.js
+++ task/task.delete.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Router = express.Router();
-const Item = require("../models/Item");
+const { Item } = require("../models");
 
 const deleteTask = Router.delete("/:id", async (req, res) => {
   const deletingTask = await Item.destroy({ where: { id: req.params.id } });
