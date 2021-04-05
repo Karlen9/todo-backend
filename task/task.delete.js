8f676fe1aed18cb6d9e9bbe3288e3646f507a6e6
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-05 13:20:40 +0300

    Delete request edited

--- task/task.delete.js
+++ task/task.delete.js
@@ -3,16 +3,9 @@ const Router = express.Router();
 const Item = require("../models");
 
 const deleteTask = Router.delete("/:id", async (req, res) => {
-  try {
-    const task = await Item.findOne({ where: { id: req.params.id } });
-    if (!task) throw new Error("Task does not exist");
+  const deletingTask = await Item.destroy({ where: { id: req.params.id } });
 
-    const deletingTask = await Item.destroy({ where: { id: req.params.id } });
-
-    res.send(deletingTask);
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
+  res.send(deletingTask);
 });
 
 module.exports = deleteTask;
--- task/task.get.js
+++ task/task.get.js
@@ -17,7 +17,7 @@ const get = Router.get("/", async (req, res) => {
   if (req.query.sort) sort = ["createdAt", req.query.sort];
   const tasks = await Item.findAndCountAll({
     where: filter,
-    order: sort[1],
+    order: sort,
     limit: 5,
     offset: (req.query.page - 1) * 5,
   });
