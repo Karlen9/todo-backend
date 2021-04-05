1e727f3dd98ab99cbecaeb6bf7382f570ae3acd1
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-05 10:53:51 +0300

    New model added

--- task/task.delete.js
+++ task/task.delete.js
@@ -1,20 +1,18 @@
-const express = require('express');
+const express = require("express");
 const Router = express.Router();
-const Task = require('../models');
+const Item = require("../models");
 
-
-const deleteTask = Router.delete('/:id', async (req, res) => {
+const deleteTask = Router.delete("/:id", async (req, res) => {
   try {
-    const task = await Task.findOne({where: {id: req.params.id}});
-    if(!task) throw new Error('Task does not exist');
+    const task = await Item.findOne({ where: { id: req.params.id } });
+    if (!task) throw new Error("Task does not exist");
 
-    const deletingTask = await Task.destroy({where: {id: req.params.id}}); 
-  
-  res.send(deletingTask);
+    const deletingTask = await Item.destroy({ where: { id: req.params.id } });
+
+    res.send(deletingTask);
   } catch (error) {
     res.status(400).json(error.message);
   }
-  
 });
 
 module.exports = deleteTask;
--- task/task.get.js
+++ task/task.get.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const Router = express.Router();
-const { Task } = require("../models");
+const { Item } = require("../models");
 
 const get = Router.get("/", async (req, res) => {
   let filter = {};
   let sort = [];
   if (req.query.filterBy) filter = { done: req.query.filterBy };
   if (req.query.sort) sort = ["createdAt", req.query.sort];
-  const tasks = await Task.findAndCountAll({
+  const tasks = await Item.findAndCountAll({
     where: filter.done,
     order: sort,
     limit: 5,
