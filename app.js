475853ee0317aaef5e72137afc1734c369a349c3
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-06 12:36:15 +0300

    Get request edited

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("start");
 });
 
--- task/task.get.js
+++ task/task.get.js
@@ -3,24 +3,26 @@ const Router = express.Router();
 const { Item } = require("../models");
 
 const get = Router.get("/", async (req, res) => {
-  let filter = {};
   let sort = [];
-  if (req.query.filterBy) {
-    if (req.body.filterBy === "all") {
-      filter = {};
-    } else if (req.body.filterBy === "done") {
-      filter = { done: true };
-    } else if (req.body.filterBy === "undone") {
-      filter = { done: false };
-    }
-  }
+
   if (req.query.sort) sort = ["createdAt", req.query.sort];
-  const tasks = await Item.findAndCountAll({
-    where: filter,
-    order: sort,
-    limit: 5,
-    offset: (req.query.page - 1) * 5,
-  });
+
+  if (req.query.filterBy) {
+    tasks = await Item.findAndCountAll({
+      where: {
+        done: req.query.filterBy,
+      },
+      order: [sort],
+      limit: 5,
+      offset: (req.query.page - 1) * 5,
+    });
+  } else if (!req.body.filterBy) {
+    tasks = await Item.findAndCountAll({
+      order: [sort],
+      limit: 5,
+      offset: (req.query.page - 1) * 5,
+    });
+  }
 
   res.send(tasks);
 });
