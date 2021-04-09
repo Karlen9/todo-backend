ed133bfe2a6c105cc31419a1a19ee7f68ca6cee8
Karlen Misakyan <misakyankarlen@gmail.com>
2021-04-09 13:49:29 +0300

    Post for user created

--- app.js
+++ app.js
@@ -28,3 +28,4 @@ app.use("/task", require("./task/task.post"));
 app.use("/task", require("./task/task.delete"));
 app.use("/tasks", require("./task/task.get"));
 app.use("/task", require("./task/task.patch"));
+app.use("/user", require("./user/user.post"));
---
+++ user/user.post.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const User = require("../models");
+
+router.post("/register", (req, res) => {
+  const user = new User({
+    id: req.body.id,
+    fname: req.body.firstName,
+    lname: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  res.send("registered");
+});
+
+module.exports = router;
